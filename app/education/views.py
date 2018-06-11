59c46f4e06e6948675c44b062915e210bfaa806a
kjs <k104agi@gmail.com>
2018-06-11 13:53:12 +0900

    장고 페이지 설정 중

---
+++ app/education/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from models import *
+
+
+def index(request):
+    context = {
+        'student': student,
+    }
+    return render(request, 'polls/index.html', context)
+
+
+def student(request, student_id):
+    response = "You're looking at the results of student %s."
+    return HttpResponse(response % student_id)
+
+
+def school(request, school_id):
+    response = "You're looking at the results of school %s."
+    return HttpResponse(response % school_id)
